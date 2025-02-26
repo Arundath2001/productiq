141f89c8d97b02f542089376efe5fda54a2d5ef4
arundath44 <arundatharun44@gmail.com>
2025-02-26 10:13:08 +0530

    4th commit

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 if(process.env.NODE_ENV==='production'){
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-    app.get('*', (req,) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, "../forntend", "dist", "index.html"));
     })
 }
