904b0b9c166e826c14159a46ec787a95c4979d58
Arundath2001 <arundatharun44@gmail.com>
2025-04-13 00:01:55 +0530

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,8 @@ const __dirname = path.resolve();
 const allowedOrigins = [
     "http://localhost:5173",
     "https://aswaqforwarder.com",
-    "https://productiq-web.onrender.com"
+    "https://productiq-web.onrender.com",
+    "https://www.aswaqforwarder.com"
   ];
   
 
