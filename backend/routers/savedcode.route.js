aabc455e0ccec1c6a834a925c8d01d85bb326f52
Arundath2001 <arundatharun44@gmail.com>
2025-05-05 19:19:40 +0530

    saved code update for show failed print

--- backend/routers/savedcode.route.js
+++ backend/routers/savedcode.route.js
@@ -1,17 +1,13 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { deleteSavedCode, getFailedPrintsForSavedCode, getPrintStatusSummaryForSavedCode, getSavedProductCode, saveProductCode } from "../controllers/savedcode.controller.js";
+import { deleteSavedCode, getSavedProductCode, saveProductCode } from "../controllers/savedcode.controller.js";
 
 const router = express.Router();
 
 router.post("/", protectRoute, saveProductCode );
 
-router.get("/", protectRoute, getSavedProductCode );
+router.get("/", getSavedProductCode );
 
 router.delete("/:codeId", protectRoute, deleteSavedCode );
-
-router.get("/:productCode/failed", protectRoute, getFailedPrintsForSavedCode);
-
-router.get("/:productCode/summary", protectRoute, getPrintStatusSummaryForSavedCode);
 
 export default router;
